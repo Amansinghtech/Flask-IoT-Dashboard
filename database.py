c5617dbce17ef4a122b79fcda41bee2f17edd73e
AmanSingh <singhaman11415@gmail.com>
2020-03-15 12:59:05 +0530

    Database Done

--- database.py
+++ database.py
@@ -31,6 +31,33 @@ class db:
             return "success"
         except Exception as e:
             print( e)
+    
+    def update_values(self, apikey, fieldname, deviceID, temp, humidity, moisture, light):
+        try:
+            self.cursor.execute("select api_key from users;")
+            output = self.cursor.fetchall()
+            dummy = []
+            for i in output:
+                dummy.append(i[0])
+            if apikey in dummy:
+                
+                query = 'insert into {0} (deviceID, temperature, humidity, moisture, light, date_time) values("{1}", {2}, {3}, {4}, {5}, now());'.format(fieldname, deviceID, temp, humidity, moisture, light)
+                self.cursor.execute(query)
+                self.db.commit()
 
+                query = 'update Node set temperature={0}, humidity={1}, moisture = {2}, light={3} where deviceID="{4}";'.format(temp, humidity, moisture, light, deviceID)
+                self.cursor.execute(query)
+                self.db.commit()
+
+                return True
+
+            else:
+                print("not available")
+
+        except Exception as e:
+            print("[ERROR!]")
+            print(e)
 #test side
-# mydb = db('aman', '192.168.56.102', 'hacker123', 'ARMS')
+mydb = db('aman', '192.168.56.102', 'hacker123', 'ARMS')
+
+# mydb.update_values("nahidegi", "Rosegarden", "ARMS12012", 10, 10 ,10, 10)
